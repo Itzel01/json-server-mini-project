96aa07b3882c5c7e8417dfec6fedeccfa18faba8
Itzel01 <itzel.areymi.rendon@gmail.com>
2021-03-01 14:36:21 -0500

    finished part 1 and 2

--- script.js
+++ script.js
@@ -8,13 +8,42 @@ console.log("Hellow")
 
  document.addEventListener("DOMContentLoaded", () => {
     let addAMovie = document.getElementById("add-a-movie");
-    let movieName = document.getElementById("movie-name");
-    let movieImg = document.getElementById("movie-URL");
+    // let movieName = document.getElementById("movie-name");
+    // let movieImg = document.getElementById("movie-URL");
     let AddMovieButton = document.getElementById("AddMovieButton");
     let movieDiv = document.getElementById("card");
     
+    fetch("http://localhost:3000/movies")
+    .then(res => res.json())
+    .then(data => displayMovies(data))
+
     addAMovie.addEventListener("submit", addMovies); 
  })
+ 
+ function displayMovies(data){
+     data.forEach(makecard)
+ }
+
+ function makecard(movie){
+    let collection = document.getElementById("collection");
+    let cardDiv = document.createElement("div");
+    let name = document.createElement("h3");
+    let img = document.createElement("img");
+    let button = document.createElement("button");
+
+    name.innerText = movie.name;
+    img.src = movie.imageURL;
+    button.innerText = "Remove"
+
+    cardDiv.classList = "card"
+    img.classList = "avatar"
+    button.classList = "btn"
+    
+    cardDiv.append(name,img, button)
+    collection.append(cardDiv)
+
+ }
+
 /**
  * when the submit event happens, grab the current value
  * make a post fetch to the db.json api 
@@ -22,23 +51,29 @@ console.log("Hellow")
  */
 
  function addMovies (e){
+    let movieName = document.getElementById("movie-name");
+    let movieImg = document.getElementById("movie-URL");
+
      e.preventDefault()
-     
-    //  let bodyData = {
-    //      header: 
 
-    //  }
-    //  let options = {
-    //      method: "POST",
-    //      body: 
-    //  }
+     let data = {"name": `${movieName.value}`, "imageURL": `${movieImg.value}`}
+     let options = {
+         method: "POST",
+         headers: {"Content-Type": "application/json"},
+         body: JSON.stringify(data)
+     }
 
-     fetch("db.json")
+     fetch("http://localhost:3000/movies", options)
      .then(response => response.json())
      .then(data => {
-         console.log(data)
+         postData(data, movieName, movieImg)
      })
-     
+ }
+
+ //DOM Manipulation
+ function postData(data){
+    makecard(data)
+
  }
 
  
